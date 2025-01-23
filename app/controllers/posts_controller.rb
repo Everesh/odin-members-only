8eca76f8a75d9d5089ebfca996312414f5981370
Everesh <everesh@proton.me>
2025-01-24 00:31:16 +0100

    Initialize Post Model, Posts Controller and set up Post routes

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+  before_action :require_login, only: [ :new, :create ]
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def require_login
+  end
+end
